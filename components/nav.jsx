8df94f0351ec51438b37f3e43026462edd34d368
Takahiro <tkhr_621@docomo.ne.jp>
2020-01-08 19:38:56 +0900

    guhtubへのリンクを削除

--- components/nav.jsx
+++ components/nav.jsx
@@ -1,12 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
 
-const links = [
-  { href: 'https://github.com/zeit/next.js', label: 'GitHub' },
-].map(link => {
-  link.key = `nav-link-${link.href}-${link.label}`
-  return link
-})
+
 
 const Nav = () => (
   <nav>
@@ -16,11 +11,6 @@ const Nav = () => (
           <a>Home</a>
         </Link>
       </li>
-      {links.map(({ key, href, label }) => (
-        <li key={key}>
-          <a href={href}>{label}</a>
-        </li>
-      ))}
       <li>
         <Link href="/">
           <a>ログイン</a>
