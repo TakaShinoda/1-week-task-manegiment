773acd57f6a8b12d4d276e21e79ff2d411552416
Takahiro <tkhr_621@docomo.ne.jp>
2020-01-14 12:07:56 +0900

    Add matrial-ui

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
-import React from 'react'
-import Head from 'next/head'
-import Link from 'next/link'
-import Nav from '../components/nav'
-import Title from '../components/title'
+import React from 'react';
+import Head from 'next/head';
+import Link from 'next/link';
+import Nav from '../components/nav';
+import Title from '../components/title';
+import Button from '@material-ui/core/Button';
 
 const Home = () => (
   <div>
@@ -22,7 +23,9 @@ const Home = () => (
       
       <div className="row card">
         <Link href='./signin'>
-          <h3>はじめる</h3>
+          <Button size="large" variant="outlined" color="primary" size="large" >
+            はじめる
+          </Button>
         </Link>  
       </div>
     </div>
@@ -37,36 +40,11 @@ const Home = () => (
         padding-top: 10px;
       }
       .row {
-        max-width: 880px;
-        margin: 80px auto 40px;
+        margin: 40px auto 40px;
         display: flex;
         flex-direction: row;
         justify-content: space-around;
       }
-      .card {
-        padding: 18px 18px 24px;
-        width: 220px;
-        text-align: left;
-        text-decoration: none;
-        color: #434343;
-        border: 1px solid #9b9b9b;
-      }
-      .card:hover {
-        border-color: #067df7;
-      }
-      .card h3 {
-        margin: 0;
-        color: #067df7;
-        text-align:center;
-        font-size: 22px;
-      }
-      .card p {
-        margin: 0;
-        padding: 12px 0 0;
-        font-size: 13px;
-        color: #333;
-      }
-
     `}</style>
   </div>
 )
